c6da69f08fe9981d9976c1bfea3880ab9ff74c2a
installer <installer@gmail.com>
2025-10-01 23:06:51 +0800

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "rCore-dev";
+
+  inputs = {
+    nixpkgs.url = "https://mirrors.cernet.edu.cn/nix-channels/nixpkgs-unstable/nixexprs.tar.xz";
+  };
+
+  outputs = {self, nixpkgs, ...}:
+  let 
+    pkgs = import nixpkgs { inherit system;};
+    system = "x86_64-linux";
+    base = pkgs.appimageTools.defaultFhsEnvArgs;
+  in {
+    devShells.x86_64-linux.default = (pkgs.buildFHSEnv (base // {
+        name = "rCore-dev";
+        targetPkgs = pkgs: (base.targetPkgs pkgs) ++ (with pkgs; [
+          qemu_full cargo-binutils gdb gdbgui
+        ]);
+        runScript = "fish";
+      })).env;
+  };
+}
